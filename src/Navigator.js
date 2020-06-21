3d6c3283bc0816e6e0d4b0c43bd413efcb7534d6
Murilo Pedroso <maverickmu@gmail.com>
2020-06-21 19:59:48 -0300

    Tela para adicionar Fotos

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,40 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
-
-const MenuRoutes = {
-  Feed: {
-    name: 'Feed',
-    screen: Feed,
-    navigationOptions: {
-      title: 'Feed',
-      tabBarIcon: ({ tintColor }) => <Icon name='home' size={30} color={tintColor} />
-    }
-  },
-  Add: {
-    name: 'AddPhoto',
-    screen: Feed,
-    navigationOptions: {
-      title: 'Add Picture',
-      tabBarIcon: ({ tintColor }) => <Icon name='camera' size={30} color={tintColor} />
-    }
-  },
-  Profile: {
-    name: 'Profile',
-    screen: Feed,
-    navigationOptions: {
-      title: 'Profile',
-      tabBarIcon: ({ tintColor }) => <Icon name='user' size={30} color={tintColor} />
-    }
-  },
-}
-
-const MenuConfig = {
-  initialRouteName: 'Feed',
-  tabBarOptions: {
-    showLabel: false
-  }
-}
+import AddPhoto from './screens/AddPhoto'
 
 const Tab = createBottomTabNavigator()
 
@@ -47,7 +14,7 @@ function MenuNavigator() {
       <Tab.Navigator initialRouteName="Feed" tabBarOptions={{ showLabel: false }} >
         <Tab.Screen name="Feed" component={Feed}
           options={{ title: 'Feed', tabBarIcon: ({ tintColor }) => <Icon name='home' size={30} color={tintColor} /> }} />
-        <Tab.Screen name="AddPhoto" component={Feed}
+        <Tab.Screen name="AddPhoto" component={AddPhoto}
           options={{ title: 'AddPhoto', tabBarIcon: ({ tintColor }) => <Icon name='camera' size={30} color={tintColor} /> }} />
         <Tab.Screen name="Profile" component={Feed}
           options={{ title: 'Profile', tabBarIcon: ({ tintColor }) => <Icon name='user' size={30} color={tintColor} /> }} />          
