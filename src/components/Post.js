c636e02f841a9ef4aa5b5546fe9ebc0d7843a5dd
Murilo Pedroso <maverickmu@gmail.com>
2020-06-21 09:44:52 -0300

    Componente Post

---
+++ src/components/Post.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import {StyleSheet, View, Image, Dimensions} from 'react-native'
+import Author from './Author'
+
+
+class Post extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={this.props.image} style={styles.image} />
+        <Author email='maverickmu@gmail.com' nickname='Murilo Pedroso' />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  image: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width * 3 / 4,
+    resizeMode : 'contain'
+  }
+})
+
+export default Post
