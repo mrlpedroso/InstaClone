ce31a5c2ffea6875dc454d68247cf94a16b35c49
Murilo Pedroso <maverickmu@gmail.com>
2020-07-05 11:32:39 -0300

    Storage do firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,45 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true})
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const {Storage} = require('@google-cloud/storage')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const storage = new Storage({
+  projectId: 'instaclone-f7b13',
+  keyFilename: 'instaclone-f7b13.json'
+})
+
+ exports.uploadImage = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    try {
+      fs.writeFileSync('/tmp/imageToSave.jpg', req.body.image, 'base64')
+
+      const bucket = storage.bucket('instaclone-f7b13.appspot.com')
+      const id = uuid()
+      bucket.upload('/tmp/imageToSave.jpg', {
+        uploadType: 'media',
+        destination: `/posts/${id}.jpg`,
+        metadata: {
+          metadata: {
+            contentType: 'image/jpeg',
+            firebaseStorageDownloadTokens: id
+          }
+        }
+      }, (err, file) => {
+        if (err) {
+          console.log(err)
+          return res.status(500).json({error: err})
+        } else {
+          const fileName = encodeURIComponent(file.name)
+          const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/' + bucket.name 
+                         + '/o/' + fileName + '?alt=media&token=' + id
+          return res.status(201).json({imageUrl: imageUrl})
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({error: err})
+    }
+    
+  })
+ });
--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -9,12 +9,25 @@ export const addPost = post => {
 //    payload: post
 //  }
 
-  return dispatch => {
-    axios.post('/posts.json', {...post})
-      .catch(err => console.log(err))
-      .then(res => console.log('teste')
-    )
-  }
+  //https://us-central1-instaclone-f7b13.cloudfunctions.net/uploadImage
+  axios({
+    url: 'uploadImage',
+    baseURL: 'https://us-central1-instaclone-f7b13.cloudfunctions.net',
+    method: 'post',
+    data: {
+      image: post.image.base64
+    }
+  })
+  .catch(err => console.log(err))
+  .then(res => {
+    return dispatch => {
+      post.image = res.data.imageUrl //substitui a imagem pela url
+      axios.post('/posts.json', {...post})
+        .catch(err => console.log(err))
+        .then(res => console.log('teste')
+      )
+    }
+  })  
 }
 
 export const addComment = payload => {
